a5701db5c25cb6ec6d655fe3744fe2f5fd3dda28
Njricu <nrice@mail.bradley.edu>
2023-04-05 20:18:45 -0500

    Fixed it
    Thx Andrea

--- Assets/Scripts/PlayerAbility.cs
+++ Assets/Scripts/PlayerAbility.cs
@@ -8,13 +8,14 @@ public class PlayerAbility : MonoBehaviour
 {
     PlayerControls actions;
 
-    private int currentAbility;
+    [SerializeField] private int currentAbility = 0;
+    private int numOfAbilities = 3;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         actions = new PlayerControls();
-        actions.Enable();
+        //actions.Enable();
     }
 
     private void AbilitySwap(int swap)
@@ -22,15 +23,37 @@ public class PlayerAbility : MonoBehaviour
         currentAbility = swap;
     }
 
+    private void SecondAbilitySwap(int swap)
+    {
+        currentAbility += swap;
+
+        if(currentAbility >= numOfAbilities)
+        {
+            currentAbility = 0;
+        }
+        else if(currentAbility < 0)
+        {
+            currentAbility = numOfAbilities - 1;
+        }
+        Debug.Log(currentAbility);
+    }
+
+    /// <summary>
+    /// This happens when the player uses the [Right Mouse Button]
+    /// It will switch case through the different abilities and select the right one ;)
+    /// </summary>
     private void AbilityUse()
     {
         switch(currentAbility)
         {
             case 0:
+
                 break;
                 case 1:
+                    
                 break;
             case 2:
+
                 break;
         }
     }
@@ -39,9 +62,19 @@ public class PlayerAbility : MonoBehaviour
     {
         actions.Enable();
 
+        // This is the item using section
+        actions.Player.AbilityUse.performed += ctx => AbilityUse();
+
+        // This would be used for stopping a hold down? 
+        // I could probably make a different one for holding something down within the controls
+        actions.Player.AbilityUse.canceled += ctx => AbilityUse();
+
+        // This is the item swapping section
         actions.Player.Item1.performed += ctx => AbilitySwap(1);
         actions.Player.Item2.performed += ctx => AbilitySwap(2);
         actions.Player.Item3.performed += ctx => AbilitySwap(3);
+
+        actions.Player.ItemScroll.performed += ctx => SecondAbilitySwap((int)ctx.ReadValue<float>() / Mathf.Abs((int)ctx.ReadValue<float>()));
     }
 
     private void OnDisable()
