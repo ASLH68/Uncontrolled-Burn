5892837e64c4a86b116fa12b2747d90f5cdb1c7d
Andrea Swihart <andreaswihartdecoster@gmail.com>
2023-04-10 13:47:33 -0500

    Replaced walls

--- Assets/Scripts/PlayerAbility.cs
+++ Assets/Scripts/PlayerAbility.cs
@@ -129,7 +129,7 @@ public class PlayerAbility : MonoBehaviour
             // This is the item using section
             actions.Player.AbilityUse.performed += ctx => AbilityUse();
 
-        actions.Player.AbilityUse.performed += ctx => Debug.Log("Howdty");
+            actions.Player.AbilityUse.performed += ctx => Debug.Log("Howdty");
 
             // This would be used for stopping a hold down? 
             // I could probably make a different one for holding something down within the controls
