5716f2188b127f1c16b86bcf632f295767515353
RiM-prfctstrm <78179573+RiM-prfctstrm@users.noreply.github.com>
2023-04-15 09:53:44 -0500

    e

--- Assets/Scripts/ToolScripts/Flamethrower.cs
+++ Assets/Scripts/ToolScripts/Flamethrower.cs
@@ -8,7 +8,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using StarterAssets;
 public class Flamethrower : MonoBehaviour
 {
     [SerializeField] GameObject _fireCone;
